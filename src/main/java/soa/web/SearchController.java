1f2a5a714741f6a9e143940e56cb0014f5f17d18
Francisco Menendez Moya <menendezmoya94@gmail.com>
2016-12-21 11:20:15 +0100

    Work for lab 5 done

--- src/main/java/soa/web/SearchController.java
+++ src/main/java/soa/web/SearchController.java
@@ -7,22 +7,35 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class SearchController {
 
-	@Autowired
-	  private ProducerTemplate producerTemplate;
+    @Autowired private ProducerTemplate producerTemplate;
 
-	@RequestMapping("/")
+    @RequestMapping("/")
     public String index() {
         return "index";
     }
 
-
     @RequestMapping(value="/search")
     @ResponseBody
     public Object search(@RequestParam("q") String q) {
-        return producerTemplate.requestBodyAndHeader("direct:search", "", "CamelTwitterKeywords", q);
+        String [] stringArray = q.split("max:");
+        Integer numberFormatted = -1;
+        try{
+            numberFormatted = Integer.parseInt(stringArray[1]);
+        }catch (Exception ignored) {}
+
+        if (stringArray.length == 2 && (numberFormatted != -1)){
+            Map<String,Object> headers = new HashMap<>();
+            headers.put("CamelTwitterKeywords", stringArray[0]);
+            headers.put("CamelTwitterCount", numberFormatted);
+            return producerTemplate.requestBodyAndHeaders("direct:search", "", headers);
+        }else{
+            return producerTemplate.requestBodyAndHeader("direct:search", "", "CamelTwitterKeywords", q);
+        }
     }
 }
